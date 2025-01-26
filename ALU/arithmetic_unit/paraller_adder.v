4c7566366fbc81fd69d7ec695197f9249b509e72
Saheel Faisal <saheelfaisal@outlook.com>
2025-01-25 19:39:19 -0600

    Added parallel adder

---
+++ ALU/arithmetic_unit/paraller_adder.v
@@ -0,0 +1,15 @@
+// Module Name: paraller_adder
+// Author: Saheel Faisal
+// Date: 2025-01-25
+
+
+module paraller_adder(
+    input [31:0] A,
+    input [31:0] Y,
+    input Cin,
+    output G
+    );
+
+    assign G = A + Y + Cin;
+
+endmodule
